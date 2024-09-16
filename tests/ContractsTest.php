973aacfe849b969b7de8f4e34ee03f415fdebe25
renzoDC15 <renzo.carianga@gmail.com>
2024-09-16 12:42:23 +0800

    add sku, wages, tcp, and promo code to reference dataset

--- tests/ContractsTest.php
+++ tests/ContractsTest.php
@@ -303,12 +303,16 @@ it('has dated status', function() {
 dataset('reference', function () {
     return [
         [fn() => app(CreateReferenceAction::class)->run([
+            InputFieldName::SKU => $this->faker->word(),
+            InputFieldName::WAGES => $this->faker->numberBetween(10000, 120000) * 1.00,
+            InputFieldName::TCP => $this->faker->numberBetween(850000, 4000000) * 1.00,
             InputFieldName::PERCENT_DP => $this->faker->numberBetween(5, 10)/100,
             InputFieldName::PERCENT_MF => $this->faker->numberBetween(8, 10)/100,
             InputFieldName::DP_TERM => $this->faker->numberBetween(12, 24) * 1.00,
             InputFieldName::BP_TERM => $this->faker->numberBetween(20, 30) * 1.00,
             InputFieldName::BP_INTEREST_RATE => $this->faker->numberBetween(3, 7)/100,
             InputFieldName::SELLER_COMMISSION_CODE => $this->faker->word(),
+            InputFieldName::PROMO_CODE => $this->faker->word(),
         ], ['author' => 'Lester'])]
     ];
 });
