53ce7cee5b2e6de120e11da15df3228467edbf57
RDC15 <34361260+renzoDC15@users.noreply.github.com>
2025-03-08 13:48:10 +0800

    add promo code

--- src/Traits/HasMiscAttributes.php
+++ src/Traits/HasMiscAttributes.php
@@ -7,10 +7,12 @@ use Homeful\Contracts\Models\Contract;
 trait HasMiscAttributes
 {
     const VOUCHER_CODE ='voucher_code';
+    const PROMO_CODE ='promo_code';
     public function initializeHasMiscAttributes(): void
     {
         $this->mergeFillable([
-            'voucher_code'
+            'voucher_code',
+            'promo_code',
         ]);
     }
 
@@ -23,6 +25,17 @@ trait HasMiscAttributes
     public function getVoucherCodeAttribute(): string{
         $default = null;
         return $this->getAttribute('meta')->get(Contract::VOUCHER_CODE) ?? $default;
+    }
+
+    public function setPromoCodeAttribute(string $voucher_code): self
+    {
+        $this->getAttribute('meta')->set(Contract::PROMO_CODE, $voucher_code);
+        return $this;
+    }
+
+    public function getPromoCodeAttribute(): string{
+        $default = null;
+        return $this->getAttribute('meta')->get(Contract::PROMO_CODE) ?? $default;
     }
 
 }
